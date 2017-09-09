82a40297244446fecc1cd8533aa79fd566e8f98d
yao <354898288@qq.com>
2017-09-09 11:28:47 +0800

    init2

---
+++ study-aop/src/main/java/com/studyaop/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.studyaop;
+
+/**
+ * Created by yao_pc on 2017/9/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
