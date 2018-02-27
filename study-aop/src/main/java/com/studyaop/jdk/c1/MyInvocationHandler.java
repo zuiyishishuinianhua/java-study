365112afc2e7349ff7cdf6dd93cb944ca5204df6
yao <354898288@qq.com>
2018-02-27 10:58:35 +0800

    fd

--- study-aop/src/main/java/com/studyaop/jdk/c1/MyInvocationHandler.java
+++ study-aop/src/main/java/com/studyaop/jdk/c1/MyInvocationHandler.java
@@ -17,6 +17,7 @@ public class MyInvocationHandler implements InvocationHandler {
     }
 
 
+    @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         System.out.println("++++++before " + method.getName() + "++++++");
         Object result = method.invoke(target, args);
